fb66c715fb4422e2e3bbfe80734c7b9e52245029
MGuerrier <marcusmguerrier@gmail.com>
2021-03-31 18:48:59 -0400

    finished PyPoll

--- PyPoll/main (finalfinal).py
+++ PyPoll/main (finalfinal).py
@@ -13,7 +13,7 @@ vote_list = []
 percent_list = []
 winner = ""
 
-# Open the CSV (Thx GitHub)
+# Open the CSV (Credit: GitHub)
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     
@@ -30,3 +30,50 @@ with open(csvpath, newline="") as csvfile:
             vote_list.append(1)
         else:
             vote_list[candidate_list.index(row[2])] += 1
+
+# Calculate votes and find the winner           
+percent_list = [(100/total_votes) * x for x in vote_list]
+winner = candidate_list[vote_list.index(max(vote_list))]
+
+# Printing results and exporting data to our text file (Thx Solutions folder)
+with open(file_to_output, "w") as txt_file:
+
+    # Printing to terminal
+    election_results = (
+        f"\n\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+
+    # Save the final vote count to the text file
+    txt_file.write(election_results)
+
+    # Determine the winner by looping through the counts
+    for candidate in candidate_votes:
+
+        # Retrieve vote count and percentage
+        votes = candidate_votes.get(candidate)
+        vote_percentage = float(votes) / float(total_votes) * 100
+
+        # Determine winning vote count and candidate
+        if (votes > winning_count):
+            winning_count = votes
+            winning_candidate = candidate
+
+        # Print each candidate's voter count and percentage (to terminal)
+        voter_output = f"{candidate}: {vote_percentage:.3f}% ({votes})\n"
+        print(voter_output, end="")
+
+        # Save each candidate's voter count and percentage to text file
+        txt_file.write(voter_output)
+
+    # Print the winning candidate (to terminal)
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
+
+    # Save the winning candidate's name to the text file
+    txt_file.write(winning_candidate_summary)
